2ada4ad1e243587ab6d9af8024abd3e7a80d2080
Ryan Bishop <rdbishop06@gmail.com>
2020-02-03 15:46:42 -0600

    refactor: class Student module

--- reports.py
+++ reports.py
@@ -1,4 +1,5 @@
 import sqlite3
+from student import Student
 
 class StudentExerciseReports():
 
@@ -12,6 +13,9 @@ class StudentExerciseReports():
         """Retrieve all students with the cohort name."""
 
         with sqlite3.connect(self.db_path) as conn:
+            conn.row_factory = lambda cursor, row: Student(
+                row[0], row[1], row[2], row[3]
+            )
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
@@ -28,7 +32,7 @@ class StudentExerciseReports():
             all_students = db_cursor.fetchall()
 
             for student in all_students:
-                print(f'{student[0]} {student[1]} is in {student[3]}.')
+                print(student)
 
 reports = StudentExerciseReports()
 reports.all_students()
