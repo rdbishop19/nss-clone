3121070c58bab47a39e1f1b7512b0b948f0c9bbc
Ryan Bishop <rdbishop06@gmail.com>
2020-02-03 10:28:49 -0600

    empty db and updated create file

--- create_tables.sql
+++ create_tables.sql
@@ -1,68 +1,68 @@
-DROP TABLE IF EXISTS students
-DROP TABLE IF EXISTS instructors
-DROP TABLE IF EXISTS cohorts
-DROP TABLE IF EXISTS exercises
-DROP TABLE IF EXISTS languages
-DROP TABLE IF EXISTS student_roster
-DROP TABLE IF EXISTS instructor_roster
-DROP TABLE IF EXISTS assignments
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS instructors;
+DROP TABLE IF EXISTS cohorts;
+DROP TABLE IF EXISTS exercises;
+DROP TABLE IF EXISTS languages;
+DROP TABLE IF EXISTS student_roster;
+DROP TABLE IF EXISTS instructor_roster;
+DROP TABLE IF EXISTS assignments;
 
-CREATE TABLE `students` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `firstName` TEXT NOT NULL,
-  `lastName` TEXT NOT NULL,
-  `slackHandle` TEXT,
-  `cohortId` INTEGER,
-  FOREIGN KEY (`cohortId`) REFERENCES `cohorts` (`id`)
+CREATE TABLE students (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  firstName TEXT NOT NULL,
+  lastName TEXT NOT NULL,
+  slackHandle TEXT,
+  cohortId INTEGER,
+  FOREIGN KEY (cohortId) REFERENCES cohorts (id)
 );
 
-CREATE TABLE `instructors` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `firstName` TEXT NOT NULL,
-  `lastName` TEXT NOT NULL,
-  `slackHandle` TEXT,
-  `speciality` TEXT
+CREATE TABLE instructors (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  firstName TEXT NOT NULL,
+  lastName TEXT NOT NULL,
+  slackHandle TEXT,
+  speciality TEXT
 );
 
-CREATE TABLE `cohorts` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `name` TEXT NOT NULL
+CREATE TABLE cohorts (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE `exercises` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `name` TEXT NOT NULL,
-  `language` INTEGER,
-  FOREIGN KEY (`language`) REFERENCES `languages` (`id`)
+CREATE TABLE exercises (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  language INTEGER,
+  FOREIGN KEY (language) REFERENCES languages (id)
 );
 
-CREATE TABLE `languages` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `name` TEXT NOT NULL
+CREATE TABLE languages (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT UNIQUE
 );
 
-CREATE TABLE `student_roster` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `studentId` INTEGER NOT NULL,
-  `cohortId` INTEGER NOT NULL,
-  FOREIGN KEY (`cohortId`) REFERENCES `cohorts` (`id`),
-  FOREIGN KEY (`studentId`) REFERENCES `students` (`id`)
+CREATE TABLE student_roster (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  studentId INTEGER NOT NULL,
+  cohortId INTEGER NOT NULL,
+  FOREIGN KEY (cohortId) REFERENCES cohorts (id),
+  FOREIGN KEY (studentId) REFERENCES students (id)
 );
 
-CREATE TABLE `instructor_roster` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `instructorId` INTEGER NOT NULL,
-  `cohortId` INTEGER NOT NULL,
-  FOREIGN KEY (`cohortId`) REFERENCES `cohorts` (`id`),
-  FOREIGN KEY (`instructorId`) REFERENCES `instructors` (`id`)
+CREATE TABLE instructor_roster (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  instructorId INTEGER NOT NULL,
+  cohortId INTEGER NOT NULL,
+  FOREIGN KEY (cohortId) REFERENCES cohorts (id),
+  FOREIGN KEY (instructorId) REFERENCES instructors (id)
 );
 
-CREATE TABLE `assignments` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `studentId` INTEGER NOT NULL,
-  `exerciseId` INTEGER NOT NULL,
-  FOREIGN KEY (`exerciseId`) REFERENCES `exercises` (`id`),
-  FOREIGN KEY (`studentId`) REFERENCES `students` (`id`)
+CREATE TABLE assignments (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  studentId INTEGER NOT NULL,
+  exerciseId INTEGER NOT NULL,
+  FOREIGN KEY (exerciseId) REFERENCES exercises (id),
+  FOREIGN KEY (studentId) REFERENCES students (id)
 );
 
 
